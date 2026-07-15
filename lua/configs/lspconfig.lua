25fd903da00ac7a0cf682cc808f3d46a7f3d84df
Nabil <faqiharif4@gmail.com>
2026-07-15 11:29:29 +0700

    remove suggestion hints

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -118,14 +118,25 @@ vim.lsp.config("gopls", {
   },
 })
 
--- -- 🔥 MATIKAN SEMANTIC TOKENS GLOBAL
--- vim.api.nvim_create_autocmd("LspAttach", {
---   callback = function(args)
---     local client = vim.lsp.get_client_by_id(args.data.client_id)
---     if client then
---       client.server_capabilities.semanticTokensProvider = nil
---     end
---   end,
--- })
+-- Sembunyikan Hint & Info virtual text
+vim.diagnostic.config {
+  virtual_text = {
+    severity = {
+      min = vim.diagnostic.severity.WARN,
+    },
+  },
+
+  signs = {
+    severity = {
+      min = vim.diagnostic.severity.WARN,
+    },
+  },
+
+  underline = {
+    severity = {
+      min = vim.diagnostic.severity.WARN,
+    },
+  },
+}
 
 -- read :h vim.lsp.config for changing options of lsp servers
