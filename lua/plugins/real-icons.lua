65a40692b0f25ef1bc9fc7d5f89089741dfddd24
Nabil <faqiharif4@gmail.com>
2026-07-22 09:04:54 +0700

    update material icon theme

--- lua/plugins/real-icons.lua
+++ lua/plugins/real-icons.lua
@@ -17,14 +17,42 @@ return {
 
       local builder = require "nvim-tree.renderer.builder"
       local default_icon_name_decorated = builder.icon_name_decorated
-      local real_icons = require "real-icons.integrations.nvim_tree"
+      local packs = require "real-icons.packs"
+      local renderer = require "real-icons.render.placeholder"
+      local resolver = require "real-icons.resolver"
+
+      local function folder_icon(node)
+        local icon = resolver.resolve("directory", node.absolute_path, { is_dir = true })
+
+        if node.open then
+          local pack = packs.get()
+          local folder_name = node.name:lower()
+          local open_key = (pack.folder_names_expanded or {})[folder_name] or pack.folder_expanded
+          local open_source = open_key and pack.definitions[open_key]
+
+          if open_source then
+            icon = vim.tbl_deep_extend("force", {}, icon, {
+              key = open_key,
+              source = open_source,
+              asset = open_source,
+              is_default = false,
+            })
+          end
+        end
+
+        local segment = renderer.segment(icon)
+        return {
+          str = segment.text,
+          hl = { segment.hl },
+        }
+      end
 
       builder.icon_name_decorated = function(self, node)
         local icon, name = default_icon_name_decorated(self, node)
         local is_directory = node and (node.type == "directory" or type(node.nodes) == "table" or node.has_children ~= nil)
 
         if is_directory then
-          local real_icon = real_icons.icon_for_node(node)
+          local real_icon = folder_icon(node)
           if real_icon then
             icon = real_icon
           end
